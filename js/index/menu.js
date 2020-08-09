3e9152dea50ba488747165d0f5c8db939e85b7d1
jj811208 <jj811208@gmail.com>
2020-08-09 17:02:05 +0800

    Updates

--- js/index/menu.js
+++ js/index/menu.js
@@ -5,21 +5,39 @@ function menuInit() {
   menuDOM.addEventListener("click", function () {
     menuDOM.classList.toggle("menu--active");
   });
-  gsap.timeline({
+  var menuAnime = gsap.timeline({
     repeat: -1,
-    duration: 2
-  }).to(".menu svg", {
-    scaleX: 0.8,
-    duration: 1,
-    yoyo: 1
+    duration: 2,
+    repeatDelay: 2
   }).to(".menu #topB", {
-    yPercent: -30,
-    duration: 1,
-    yoyo: 1
-  }).to(".menu #bottomB", {
-    yPercent: 30,
-    duration: 1,
-    yoyo: 1
+    ease: Power2.easeOut,
+    yPercent: -20,
+    duration: 0.8
+  }, 0).to(".menu #bottomB", {
+    ease: Power2.easeOut,
+    yPercent: 40,
+    duration: 0.8
+  }, 0).to(".menu #topB,.menu #bottomB", {
+    ease: Elastic.easeOut.config(1.5, 0.2),
+    yPercent: 0,
+    duration: 1.2
+  }, 0.8);
+  console.log(menuAnime);
+  mouseHover(".menu", {
+    mouseover: function mouseover() {
+      menuAnime.pause();
+      return gsap.to(".menu svg", {
+        yPercent: -12,
+        duration: 0.2
+      });
+    },
+    mouseout: function mouseout() {
+      menuAnime.play();
+      gsap.to(".menu svg", {
+        yPercent: 0,
+        duration: 0.2
+      });
+    }
   });
 } // #topB
 // #centerM
